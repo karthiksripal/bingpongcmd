9676e755aad70c956340ac1c5b18d28b044f95ae
Aaron <rosenbergerga1@gcc.edu>
2015-03-17 23:35:37 -0400

    Minor changes
    Pause at end for debug/refine purposes
    Snarky echo
    Changed wait time to 2 seconds

--- bing.bat
+++ bing.bat
@@ -2,12 +2,13 @@
 CLS
 ECHO Killing Chrome processes.
 :killchrome
+ECHO Attempting to murder a process...
 taskkill /F /IM chrome.exe /T >nul 2>&1
-timeout /t 1
+timeout /t 2
 tasklist | findstr -i chrome.exe
 if %errorlevel% equ 0 goto killchrome
 ECHO Chrome processes killed.
-timeout /t 1
+timeout /t 2
 ECHO Starting Google Chrome with --disable-web-security at bing-pong.com.
 start /b "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security bing-pong.com
-goto:eof
+pause
