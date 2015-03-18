f90603c0fc4e7fb6188dcd94070bb6756c05c7f6
Aaron <rosenbergerga1@gcc.edu>
2015-03-17 22:20:16 -0400

    Command Prompt exits when it's finished

--- bing.bat
+++ bing.bat
@@ -7,3 +7,4 @@ ping 1.1.1.1 -n 1 -w 1000 > nul
 ECHO Starting Google Chrome with --disable-web-security at bing-pong.com.
 start /min "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security bing-pong.com
 goto:eof
+exit
