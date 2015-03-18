b3119558f4e1b2e831c0a0b789069e8174006249
Aaron <rosenbergerga1@gcc.edu>
2015-03-17 23:26:34 -0400

    Add taskkill loop
    thanks /u/CyberInferno

--- bing.bat
+++ bing.bat
@@ -1,9 +1,13 @@
 @echo off
 CLS
 ECHO Killing Chrome processes.
+:killchrome
 taskkill /F /IM chrome.exe /T >nul 2>&1
+timeout /t 1
+tasklist | findstr -i chrome.exe
+if %errorlevel% equ 0 goto killchrome
 ECHO Chrome processes killed.
-ping 1.1.1.1 -n 1 -w 1000 > nul
+timeout /t 1
 ECHO Starting Google Chrome with --disable-web-security at bing-pong.com.
 start /b "Chrome" "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --disable-web-security bing-pong.com
 goto:eof
